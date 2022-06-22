5aa1c435806f07447d8df9a764e00f903c80a959
LUCIFER <72453415+1uci@users.noreply.github.com>
2022-06-22 14:23:41 +0430

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from plugins.nopm import User
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+
+Bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins"),
+)
+if not os.path.isdir("./downloads"):
+    os.makedirs("./downloads")
+
+Bot.start()
+User.start()
+print("\n[INFO] - STARTED VIDEO PLAYER BOT, By @Lucif3r23 !")
+
+idle()
+Bot.stop()
+User.stop()
+print("\n[INFO] - STOPPED VIDEO PLAYER BOT, By @Lucif3r23 !")
